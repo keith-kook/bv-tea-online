cd50a31aba213109f6713fbc115c8e2df79c2af5
kook <whatup.kook@gmail.com>
2021-12-16 13:38:07 +1100

    added header with logo and links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
 import './App.css'
+import Header from './components/header/header.component'
 
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shoppage/shop.component'
@@ -8,6 +9,7 @@ import ShopPage from './pages/shoppage/shop.component'
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
